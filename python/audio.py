b9cf7d149bcaae896243a50612dc3e9b1763ccf3
dreamingbird88 <jeffnju@gmail.com>
2016-06-21 20:21:46 -0400

    Update audio.py

--- python/audio.py
+++ python/audio.py
@@ -13,10 +13,53 @@ sudo apt-add-repository ppa:samrog131/ffmpeg
 
 sudo apt-get install ffmpeg
 
+run /mnt/hgfs/Data/github/programming/python/audio.py
+
 """
 
 from pydub import AudioSegment
+import os
 
-song = AudioSegment.from_wav("never_gonna_give_you_up.wav")
+def SplitMp3File(in_file, num_min=5, epsilon=0.5):
+    """
+    Read the mp3 file (in_file) and split it into pieces with the given minutes.
+    Output files will be have suffice "_d" where d starts from 0.
+    Args:
+      in_file: the input mp3 file.
+      num_min: the length (number of minutes) of each piece.
+      epsilon: the last piece minimum length == num_min * epsilon.
+    """
+    song = AudioSegment.from_mp3(in_file)
+    total = len(song)
+    step = round(num_min * 60 * 1000)
+    min_len = step * epsilon
+    # Get the suffix length.
+    suffix_len = 1
+    t = total / step
+    while t >= 10:
+        suffix_len += 1
+        t /= 10
+    suffix_len = r'%' + str(suffix_len) + 'd'
+    # Split file.
+    start = 0
+    end = step
+    i = 0
+    out_file = in_file[:-4]
+    if not os.path.isdir(out_file): os.mkdir(out_file)
+    out_file = out_file + r'/%s.mp3'
+    while total - end > min_len:
+        n = (suffix_len % i).replace(' ', '0')
+        slip = song[start:end]
+        slip.export(out_file % n, format="mp3")
+        start = end
+        end += step
+        i += 1
+    if start != 0 and total != end:
+        n = (suffix_len % i).replace(' ', '0')
+        slip = song[start:total]
+        slip.export(out_file % n, format="mp3")
 
 
+in_file = r"/mnt/hgfs/Data/Download/LuoJiShiWei/2015 (%d).mp3"
+
+for d in range(1,7): SplitMp3File(in_file % d)
